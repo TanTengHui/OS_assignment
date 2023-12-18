07aa4d17233aa4e1175694271022c7a8072ef7f0
TanTengHui <108224869+TanTengHui@users.noreply.github.com>
2023-12-18 22:23:15 +0800

    Update Main.java

--- Assignment/Main.java
+++ Assignment/Main.java
@@ -3,6 +3,7 @@
 public class Main {
     public static void main(String[] args) {
         //new NonPreemptiveSJF();
-       new PreemptiveSJF();
+        //new PreemptiveSJF();
+        // NonPreemptivePriority nonPreemptivePriority = new NonPreemptivePriority();
     }
 }
