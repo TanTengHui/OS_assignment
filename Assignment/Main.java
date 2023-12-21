f28afe88753c4403623a026bdc24bb19ba2491a9
TanTengHui <108224869+TanTengHui@users.noreply.github.com>
2023-12-21 11:00:56 +0800

    make some small changes

--- Assignment/Main.java
+++ Assignment/Main.java
@@ -4,6 +4,7 @@ public class Main {
     public static void main(String[] args) {
         //new NonPreemptiveSJF();
         //new PreemptiveSJF();
-        // NonPreemptivePriority nonPreemptivePriority = new NonPreemptivePriority();
+        //new NonPreemptivePriority();
+        
     }
 }
