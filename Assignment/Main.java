0aed1097e41bc8f8ef9b87177d52317003b67c5e
TanTengHui <1211102289@student.mmu.edu.my>
2023-12-21 11:05:41 +0800

    test something

--- Assignment/Main.java
+++ Assignment/Main.java
@@ -4,7 +4,7 @@ public class Main {
     public static void main(String[] args) {
         //new NonPreemptiveSJF();
         //new PreemptiveSJF();
-        //new NonPreemptivePriority();
+        new NonPreemptivePriority();
         
     }
 }
