3f94dd654a8f31b295803277e39bb9a9be42aa93
TanTengHui <1211102289@student.mmu.edu.my>
2024-01-03 08:55:17 +0800

    updated main

--- Assignment/Main.java
+++ Assignment/Main.java
@@ -1,36 +1,55 @@
 import java.util.*;
 
-
-
 public class Main {
+    private static final String[] MODES = {
+        "NonPreemptiveSJF",
+        "PreemptiveSJF",
+        "NonPreemptivePriority",
+        "RoundRobin"
+    };
+
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-        int type = 0;
+        int type;
 
         do {
-            System.out.print("Please select mode: \n" +
-                                "1. NonPreemptiveSJF\n" +
-                                "2. PreemptiveSJF\n" +
-                                "3. NonPreemptivePriority\n" +
-                                "4. RoundRobin\n\n" +
-                                "Input (0 to quit) -> ");
-            
-            try {
-                type = input.nextInt();
-            } catch (Exception e) {
-                System.out.println("\nError: Input is not an integer\n\n");
+            do {
+                System.out.println("Please select mode: ");
+                for (int i = 0; i < MODES.length; i++) {
+                    System.out.printf("%d. %s\n", i + 1, MODES[i]);
+                }
+                System.out.print("\nInput (0 to quit) -> ");
+
+                try {
+                    type = input.nextInt();
+                } catch (Exception e) {
+                    System.out.println("\nError: Input is not an integer\n\n");
+                    input.nextLine(); // Clear the input buffer
+                    continue;
+                }
+
+                if (type < 0 || type > MODES.length) System.out.println("\nPlease choose the correct option\n\n");
+                else break;
+            } while (true);
+
+            switch (type) {
+                case 0:
+                    System.exit(0);
+                case 1:
+                    new NonPreemptiveSJF();
+                    break;
+                case 2:
+                    new PreemptiveSJF();
+                    break;
+                case 3:
+                    new NonPreemptivePriority();
+                    break;
+                case 4:
+                    new RoundRobin();
+                    break;
+                default:
+                    System.out.println("\nPlease choose the correct option\n\n");
             }
-
-            if (type == 0) System.exit(0);
-            else if (!(type == 1 || type == 2 || type ==3 || type == 4)) System.out.println("\nPlease choose the correct option\n\n");
-            else break;
-        } while (true);
-        
-        if (type == 1) new NonPreemptiveSJF();
-        else if (type == 2) new PreemptiveSJF();
-        else if (type == 3) new NonPreemptivePriority();
-        else if (type == 4) new RoundRobin();
-
-        input.close();
+        } while (type != 0);
     }
 }
