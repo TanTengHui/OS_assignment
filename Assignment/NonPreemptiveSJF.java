372b7bf0efeaa89336e5fd213170f3a2f97153f6
TanTengHui <1211102289@student.mmu.edu.my>
2024-01-24 12:54:49 +0800

    redo whole code

--- Assignment/NonPreemptiveSJF.java
+++ Assignment/NonPreemptiveSJF.java
@@ -1,111 +1,115 @@
 import java.text.DecimalFormat;
+import java.util.Arrays;
 import java.util.Scanner;
+
+class Process {
+    int pid; // Process ID
+    int at; // Arrival Time
+    int bt; // Burst Time
+    int ct, wt, tat, start_time; // Completion Time, Waiting Time, Turn Around Time, start time
+}
 
 public class NonPreemptiveSJF {
     public NonPreemptiveSJF() {
-        executeSJFScheduling();
+        executeSJF();
     }
 
-    private void executeSJFScheduling() {
+    private void executeSJF() {
         try (Scanner scanner = new Scanner(System.in)) {
-            int numProcesses;
+            int n;
+            boolean[] is_completed;
+            int current_time = 0;
+            int completed = 0;
+
+            int max_completion_time, min_arrival_time;
+
             while (true) {
-                System.out.print("Enter the number of processes (3-10): ");
-                numProcesses = scanner.nextInt();
-                if (numProcesses >= 3 && numProcesses <= 10) {
+                System.out.print("Enter total number of processes (3-10): ");
+                n = scanner.nextInt();
+                if (n >= 3 && n <= 10) {
                     break; // Break the loop if the input is within the valid range
                 } else {
                     System.out.println("Number of processes should be between 3 and 10. Please try again.");
                 }
             }
+            is_completed = new boolean[n];
+            Arrays.fill(is_completed, false);
 
-            int[] arrivalTime = new int[numProcesses];
-            int[] burstTime = new int[numProcesses];
-            int[] waitingTime = new int[numProcesses];
-            int[] turnaroundTime = new int[numProcesses];
-            boolean[] completed = new boolean[numProcesses];
-
-            for (int i = 0; i < numProcesses; i++) {
-                System.out.println("Enter details for Process P" + i + ":");
-                System.out.print("Arrival Time: ");
-                arrivalTime[i] = scanner.nextInt();
-                System.out.print("Burst Time: ");
-                burstTime[i] = scanner.nextInt();
-                completed[i] = false;
+            Process[] ps = new Process[n];
+            for (int i = 0; i < n; i++) {
+                ps[i] = new Process();
+                ps[i].pid = i;
+                System.out.print("\nEnter Process " + i + " Arrival Time: ");
+                ps[i].at = scanner.nextInt();
+                System.out.print("\nEnter Process " + i + " Burst Time: ");
+                ps[i].bt = scanner.nextInt();
             }
 
-            int currentTime = 0;
-            int totalTurnaroundTime = 0;
-            int totalWaitingTime = 0;
-
-            System.out.println("\nGantt Chart:");
-
+            // Variables for Gantt chart
             StringBuilder ganttChart = new StringBuilder("|");
             StringBuilder upperLine = new StringBuilder("-");
             StringBuilder lowerLine = new StringBuilder("-");
+            int timeUnitWidth = 2; // Constant width for each time unit in the Gantt chart
 
-            // Define constant width for each time unit in the Gantt chart
-            int timeUnitWidth = 5;
+            int sum_tat = 0, sum_wt = 0;
+            while (completed != n) {
+                int min_index = -1;
+                int minimum = Integer.MAX_VALUE;
 
-            while (true) {
-                int shortest = -1;
-                int shortestBurst = Integer.MAX_VALUE;
-
-                for (int i = 0; i < numProcesses; i++) {
-                    if (!completed[i] && arrivalTime[i] <= currentTime && burstTime[i] < shortestBurst) {
-                        shortest = i;
-                        shortestBurst = burstTime[i];
-                    }
-                }
-
-                if (shortest == -1) {
-                    boolean allCompleted = true;
-                    for (int i = 0; i < numProcesses; i++) {
-                        if (!completed[i]) {
-                            allCompleted = false;
-                            break;
+                for (int i = 0; i < n; i++) {
+                    if (ps[i].at <= current_time && !is_completed[i]) {
+                        if (ps[i].bt < minimum) {
+                            minimum = ps[i].bt;
+                            min_index = i;
+                        }
+                        if (ps[i].bt == minimum) {
+                            if (ps[i].at < ps[min_index].at) {
+                                minimum = ps[i].bt;
+                                min_index = i;
+                            }
                         }
                     }
-                    if (allCompleted) {
-                        break;
-                    } else {
-                        currentTime++;
-                        continue;
-                    }
                 }
-                upperLine.append("-".repeat(Math.max(0, timeUnitWidth))).append("------");
-                ganttChart.append("P").append(shortest).append(" ".repeat(Math.max(0, timeUnitWidth - 1))).append("|");
-                lowerLine.append("-".repeat(Math.max(0, timeUnitWidth))).append("------");
 
-                currentTime += burstTime[shortest];
+                if (min_index == -1) {
+                    current_time++;
+                } else {
 
-                turnaroundTime[shortest] = currentTime - arrivalTime[shortest];
-                waitingTime[shortest] = turnaroundTime[shortest] - burstTime[shortest];
-                totalTurnaroundTime += turnaroundTime[shortest];
-                totalWaitingTime += waitingTime[shortest];
-                completed[shortest] = true;
+                    upperLine.append("-".repeat(Math.max(0, timeUnitWidth))).append("------");
+                    ganttChart.append("P").append(ps[min_index].pid)
+                            .append(" ".repeat(Math.max(0, timeUnitWidth - 1))).append("|");
+                    lowerLine.append("-".repeat(Math.max(0, timeUnitWidth))).append("------");
+                    ps[min_index].start_time = current_time;
+                    ps[min_index].ct = ps[min_index].start_time + ps[min_index].bt;
+                    ps[min_index].tat = ps[min_index].ct - ps[min_index].at;
+                    ps[min_index].wt = ps[min_index].tat - ps[min_index].bt;
+
+                    sum_tat += ps[min_index].tat;
+                    sum_wt += ps[min_index].wt;
+
+                    completed++;
+                    is_completed[min_index] = true;
+                    current_time = ps[min_index].ct;
+
+                }
+            }
+
+            max_completion_time = Integer.MIN_VALUE;
+            min_arrival_time = Integer.MAX_VALUE;
+            for (int i = 0; i < n; i++) {
+                max_completion_time = Math.max(max_completion_time, ps[i].ct);
+                min_arrival_time = Math.min(min_arrival_time, ps[i].at);
             }
             int chartLength = ganttChart.length();
             upperLine.setLength(chartLength);
             lowerLine.setLength(chartLength);
+
+            System.out.println("\nGantt Chart:");
             System.out.println(upperLine);
-            System.out.println(ganttChart.toString());
+            System.out.println(ganttChart);
             System.out.println(lowerLine);
 
-           
-           
-            // System.out.print("0 ");
-            // for (int i = 0; i < numProcesses; i++) {
-            //     for (int j = 0; j < burstTime[i]; j++) {
-            //         System.out.print(" ");
-            //     }
-
-            //     int finishTime = arrivalTime[i] + turnaroundTime[i];
-            //     System.out.print(finishTime);
-            // }
-
-            System.out.println();
-
+            // Output the Process Details table
             System.out.println("\nProcess Details:");
             System.out.println(
                     "+----------+------------------+------------------+------------------+------------------+------------------+");
@@ -115,23 +119,23 @@ public class NonPreemptiveSJF {
             System.out.println(
                     "+----------+------------------+------------------+------------------+------------------+------------------+");
 
-            for (int i = 0; i < numProcesses; i++) {
-                int finishTime = arrivalTime[i] + turnaroundTime[i];
+            for (int i = 0; i < n; i++) {
                 System.out.printf("| %-8s | %-16s | %-16s | %-16s | %-16s | %-16s |\n",
-                        "P" + i, arrivalTime[i], burstTime[i], finishTime, turnaroundTime[i], waitingTime[i]);
+                        "P" + ps[i].pid, ps[i].at, ps[i].bt, ps[i].ct, ps[i].tat, ps[i].wt);
             }
 
             System.out.println(
                     "+----------+------------------+------------------+------------------+------------------+------------------+");
 
-            double avgTurnaroundTime = (double) totalTurnaroundTime / numProcesses;
-            double avgWaitingTime = (double) totalWaitingTime / numProcesses;
             DecimalFormat df = new DecimalFormat("#.##");
 
-            System.out.println("\nTotal Turnaround Time: " + totalTurnaroundTime);
-            System.out.println("Average Turnaround Time: " + df.format(avgTurnaroundTime));
-            System.out.println("Total Waiting Time: " + totalWaitingTime);
-            System.out.println("Average Waiting Time: " + df.format(avgWaitingTime));
+            System.out.println("\nTotal Turnaround Time= " + sum_tat);
+            System.out.println("Average Turnaround time= " + df.format((float) sum_tat / n));
+            System.out.println("Total Waiting Time= " + sum_wt);
+            System.out.println("Average Waiting Time= " + df.format((float) sum_wt / n));
+
         }
+        ;
+
     }
 }
